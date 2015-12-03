326fbbf919ad9384653b2e761afc978c0bf16417
刘畅 <liuchang@liuchangdeMacBook-Air.local>
2015-12-03 15:48:58 +0800

    fs

--- Web/protected/views/registered/_js.php
+++ Web/protected/views/registered/_js.php
@@ -2,9 +2,9 @@ $(function() {
 	var $banner = $('.banner'),
     $unslider = $banner.unslider({dots:true,}),
     data = $unslider.data('unslider');
-    $banner.hammer().on('swiperight',function(){
+    $banner.hammer().on('swiperight panright',function(){
     	data.prev();
-    }).on('swipeleft',function(){
+    }).on('swipeleft panleft',function(){
     	data.next();
     });
 });
