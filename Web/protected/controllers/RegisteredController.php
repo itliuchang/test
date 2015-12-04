99ad80953333ae835b604ccfe61848edfd7caa2e
刘畅 <liuchang@liuchangdeMacBook-Air.local>
2015-12-04 18:33:53 +0800

    fs

--- Web/protected/controllers/RegisteredController.php
+++ Web/protected/controllers/RegisteredController.php
@@ -3,6 +3,7 @@ class RegisteredController extends Controller{
 	public function actions(){
 		return array(
 				'index' => 'application.controllers.registered.IndexAction',
+				'basicinfo' => 'application.controllers.registered.BasicInfoAction'
 			);
 	}
 }
---
+++ Web/protected/views/registered/basicInfo.php
@@ -0,0 +1,29 @@
+<div id="basicInfo">
+	<h2>BASIC INFO</h2>
+	<div class="inputOption">
+		<label class="smallTitle">Full Name</label>
+		<input type="text" value="">
+	</div>
+	<div class="inputOption">
+		<label>Phone Number</label>
+		<input type="phone" value="">
+		<input type="number">
+		<a href="#">CODE</a>
+	</div>
+	<div class="inputOption">
+		<label>I AM A...</label>
+		<select>
+			<option></option>
+		</select>
+	</div>
+	<div class="inputOption">
+		<label>LOG IN INFORMATION</label>
+		<label>Email</label>
+		<input type="email" value="" placeholder="Create a Email">
+		<input type="text" placeholder="Create a password">
+	</div>
+	<div class="footer">
+		<a href="#">BACK</a>
+		<a href="#">NEXT</a>
+	</div>
+</div>
