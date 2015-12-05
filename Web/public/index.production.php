d474367cb0d31911b59cf029983bc5377d744a39
gooss <goossonline@gmail.com>
2015-12-06 00:02:42 +0800

    add product index.php

---
+++ Web/public/index.production.php
@@ -0,0 +1,12 @@
+<?php
+
+date_default_timezone_set("Asia/Shanghai");
+
+$yii = dirname(__FILE__) . '/../protected/library/framework/yii.php';
+$config = dirname(__FILE__).'/../protected/config/main.php';
+
+defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', 0);
+defined('YII_ENV') or define('YII_ENV', 'production');
+
+require_once($yii);
+Yii::createWebApplication($config)->run();
