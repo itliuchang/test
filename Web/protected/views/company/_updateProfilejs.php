44dc56fa7b248bcb410b153eab69a14552f59539
刘畅 <liuchang@liuchangdeMacBook-Air.local>
2015-12-08 13:26:05 +0800

    fs

--- Web/protected/views/company/_updateProfilejs.php
+++ Web/protected/views/company/_updateProfilejs.php
@@ -1,9 +1,11 @@
 $(function(){
+	myscroll=new iScroll("wrapper");
 	$('.footer').click(function(){
 		$('.servicelist').removeClass('hide');
+		myscroll.refresh();
 	});
 	$('.outer>li>p').click(function(){
 		$(this).next('.inner').toggleClass('hide');
+		myscroll.refresh();
 	});
-	myscroll=new iScroll("wrapper");
 });
