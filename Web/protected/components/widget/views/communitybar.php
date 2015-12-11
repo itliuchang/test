208ecf17b3427766dfcb80ed6a0eb6a9f88e3797
刘畅 <liuchang@liuchangdeMacBook-Air.local>
2015-12-11 12:01:39 +0800

    fs

--- Web/protected/components/widget/views/communitybar.php
+++ Web/protected/components/widget/views/communitybar.php
@@ -1,5 +1,3 @@
-<div id="head" class="community">
 	<a class="col-xs-4">Services</a>
-	<a class="col-xs-4 now">Companies</a>
+	<a class="col-xs-4 now nolborder norborder">Companies</a>
 	<a class="col-xs-4">Members</a>
-</div>
--- Web/protected/views/post/newlist.php
+++ Web/protected/views/post/newlist.php
@@ -1,4 +1,7 @@
 
+<div id="head" class="community">
+	<?php $this->widget('CommunityBarWidget') ?>
+</div>
 <div class="container-fluid tbar" id="footer">
     <?php $this->widget('FTBarWidget') ?>
 </div>
