9c62d3a6f3d24a1c98733ab4fcdb165613ec5823
刘畅 <liuchang@liuchangdeMacBook-Air.local>
2015-12-16 13:53:35 +0800

    fs

--- Web/protected/controllers/registered/IndexAction.php
+++ Web/protected/controllers/registered/IndexAction.php
@@ -1,7 +1,12 @@
 <?php
 class IndexAction extends CAction{
 	public function run(){
-		$this->controller->bodyCss='access';
-		$this->controller->render('index');
+		if(Yii::app()->user->isGuest){
+			$this->controller->bodyCss='access';
+			$this->controller->render('index');
+		}else{
+			$this->controller->redirect(Assist::getDefaultURL());
+		}
+		
 	}
 }
