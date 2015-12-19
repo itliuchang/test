003c707f4c5daf6927b588e9e977973c7e870f4f
刘畅 <liuchang@liuchangdeMacBook-Air.local>
2015-12-19 17:01:22 +0800

    fs

--- Web/protected/controllers/OrderController.php
+++ Web/protected/controllers/OrderController.php
@@ -21,7 +21,7 @@ class OrderController extends Controller{
         $input->SetTime_start(date('YmdHis'));
         $input->SetTime_expire('20151230091010');
         $input->SetProduct_id($productType);
-        $input->SetNotify_url('http://www.baidu.com');
+        $input->SetNotify_url(Yii::app()->request->getHostInfo() . $wechat['payment']['notify']);
         $input->SetTrade_type('JSAPI');
         $input->SetOpenid($openid);
         $bill = WxPayApi::unifiedOrder($input);
