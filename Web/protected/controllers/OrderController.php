3d0e7717db1039bb5e305b8e01f84e27622fed4f
刘畅 <liuchang@liuchangdeMacBook-Air.local>
2015-12-21 10:15:15 +0800

    fs

--- Web/protected/controllers/OrderController.php
+++ Web/protected/controllers/OrderController.php
@@ -20,9 +20,9 @@ class OrderController extends Controller{
         $input->SetOut_trade_no((string)$orderId);
         $input->SetTotal_fee($productPrice*$productNum);
         $input->SetTime_start(date('YmdHis'));
-        $input->SetTime_expire('20151230091010');
+        $input->SetTime_expire(Assist::getOrderExpireTime(date()));
         $input->SetProduct_id($productType);
-        $input->SetNotify_url(Yii::app()->request->getHostInfo() . $wechat['payment']['notify']);
+        $input->SetNotify_url('http://hubapp.livenaked.com' . $wechat['payment']['notify']);
         $input->SetTrade_type('JSAPI');
         $input->SetOpenid($openid);
         $bill = WxPayApi::unifiedOrder($input);
