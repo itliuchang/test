71a0b5706a9f3535a64a8e391ba496a5729a588d
刘畅 <liuchang@liuchangdeMacBook-Air.local>
2015-12-27 10:39:25 +0800

    fs

--- Web/protected/controllers/registered/BasicInfoAction.php
+++ Web/protected/controllers/registered/BasicInfoAction.php
@@ -1,9 +1,9 @@
 <?php
 class BasicInfoAction extends CAction{
 	public function run(){
-		$order = new COrder;
-        $result = $order->getlist();
-        print_r($result);die;
+		// $order = new COrder;
+  //       $result = $order->getlist();
+  //       print_r($result);die;
 		if(Yii::app()->user->isGuest) {
 			$this->controller->render('basicInfo');
 		} else {
