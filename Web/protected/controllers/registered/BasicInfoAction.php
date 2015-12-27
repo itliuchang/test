dd87335d6b359f5a61e895ef708ea07544b7ba78
gooss <goossonline@gmail.com>
2015-12-27 15:06:52 +0800

    cc

--- Web/protected/controllers/registered/BasicInfoAction.php
+++ Web/protected/controllers/registered/BasicInfoAction.php
@@ -1,8 +1,6 @@
 <?php
 class BasicInfoAction extends CAction{
 	public function run(){
-			$order = new CCommunity;
-       $result = $order->getCompanyListByService();
 		if(Yii::app()->user->isGuest) {
 			$this->controller->render('basicInfo');
 		} else {
