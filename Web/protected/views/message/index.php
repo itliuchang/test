3fca15bf42344e94059e70783aa68df13b544adf
gooss <goossonline@gmail.com>
2015-12-27 18:21:38 +0800

    cc

--- Web/protected/views/message/index.php
+++ Web/protected/views/message/index.php
@@ -2,17 +2,35 @@
     <?php foreach($data as $item): ?>
 		<div class="option">
 		    <?php if($item['id2'] == 0): ?>
-				<a href="message/" class="link"></a>
-				<a href="#"><img src="/images/portrait-default.png" alt=""></a>
+				<a href="message/0/chat" class="link"></a>
+				<a href="javascript:void(0);">
+				  <img src="<?php echo $item['u2portrait']?>" onerror="this.src='/images/portrait-default.png'" alt="">
+				</a>
+				<div class="content">
+					<h3 class="overflow-line1"><?php echo '系统消息' ?></h3>
+					<p class="overflow-line1"><?php echo CHtml::encode($item['lastMsg']) ?></p>
+				</div>
 			<?php elseif($item['id2'] == Yii::app()->user->id): ?>
+				<a href="message/<?php echo $item['id1'] ?>/chat" class="link"></a>
+				<a href="javascript:void(0);">
+				  <img src="<?php echo $item['u1portrait']?>" onerror="this.src='/images/portrait-default.png'" alt="">
+				</a>
+				<div class="content">
+					<h3 class="overflow-line1"><?php echo CHtml::encode($item['u1name']) ?></h3>
+					<p class="overflow-line1"><?php echo CHtml::encode($item['lastMsg']) ?></p>
+				</div>
 			<?php else: ?>
+				<a href="message/<?php echo $item['id2'] ?>/chat" class="link"></a>
+				<a href="javascript:void(0);">
+				  <img src="<?php echo $item['u2portrait']?>" onerror="this.src='/images/portrait-default.png'" alt="">
+				</a>
+				<div class="content">
+					<h3 class="overflow-line1"><?php echo CHtml::encode($item['u2name']) ?></h3>
+					<p class="overflow-line1"><?php echo CHtml::encode($item['lastMsg']) ?></p>
+				</div>
 			<?php endif; ?>
 			
-			<div class="content">
-				<h3 class="overflow-line1">naked Retreats</h3>
-				<p class="overflow-line1">Hello Chinese members,in case </p>
-			</div>
-			<p class="time">17h</p>
+			<p class="time"><?php echo CDate::dgm($item['utime']) ?></p>
 		</div>
 	<?php endforeach; ?>
 </div>
