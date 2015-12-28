efb17d63d2614f335bbc9f3e0e160f34a8404a1a
刘畅 <liuchang@liuchangdeMacBook-Air.local>
2015-12-28 19:11:54 +0800

    fs

--- Web/protected/controllers/WechatController.php
+++ Web/protected/controllers/WechatController.php
@@ -3,7 +3,8 @@ class WechatController extends CController{
 	public function actions(){
 		return array(
 			'wechatconnect' => 'application.controllers.wechat.WechatConnectAction',
-            'wechatconnectcallback' => 'application.controllers.wechat.WechatConnectCallbackAction',		
+            'wechatconnectcallback' => 'application.controllers.wechat.WechatConnectCallbackAction',
+            'login' => 'application.controllers.wechat.LoginAction'		
 		);
 	}
 }
