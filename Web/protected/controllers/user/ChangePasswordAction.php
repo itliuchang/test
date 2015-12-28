7662f1a2551f324f7f0898e84ebc7022c052541c
yuxiangping <eason.xp.yu@gmail.com>
2015-12-28 09:58:05 +0800

    update password

--- Web/protected/controllers/user/ChangePasswordAction.php
+++ Web/protected/controllers/user/ChangePasswordAction.php
@@ -1,6 +1,21 @@
 <?php
 class ChangePasswordAction extends CAction{
 	public function run(){
-		$this->controller->render('changepassword');
+		if(Yii::app()->request->isAjaxRequest){
+			$id = Yii::app()->user->id;
+			$user = User::model()->findByAttributes(array('id'=>$id));
+			
+			$oldPassword = Yii::app()->request->getParam('oldPassword');
+			if($user->validatePassword($oldPassword)){
+				$user->password = Yii::app()->request->getParam('password');
+					
+				$user->save();
+				echo CJSON::encode(array('code'=>200, 'message'=> 'SUCCESS'));
+			} else {
+				echo CJSON::encode(array('code'=>500, 'message'=> '旧密码不正确'));
+			}
+		} else {
+			$this->controller->render('changepassword');
+		}
 	}
 }
