2d35838731b580ee5467c8b3fc9ed33e715f4acf
刘畅 <liuchang@liuchangdeMacBook-Air.local>
2015-12-29 13:07:32 +0800

    fs

--- Web/protected/controllers/user/LogoutAction.php
+++ Web/protected/controllers/user/LogoutAction.php
@@ -4,9 +4,6 @@ class LogoutAction extends CAction{
 	public function run(){
 		if(!Yii::app()->user->isGuest){
 			if(Yii::app()->session['wechat']){
-				$wechat = Account::model()->findByAttributes(array('account'=>Yii::app()->session['wechat']['unionid']));
-				$wechat->status=0;
-				$wechat->save();
 				Yii::app()->session['wechat']=null;
 			}
 			$_identity = new UserIdentity();
