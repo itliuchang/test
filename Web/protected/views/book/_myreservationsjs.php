45aa6401bb38893755895f2831898034c05cfcaf
刘畅 <liuchang@liuchangdeMacBook-Air.local>
2015-12-30 12:34:01 +0800

    fs

--- Web/protected/views/book/_myreservationsjs.php
+++ Web/protected/views/book/_myreservationsjs.php
@@ -1,13 +1,16 @@
 $(function(){
-	$('.p,.u').hammer().on('tap',function(){
+	$('.p,.u').hammer().on('tap',function(e){
+		e.gesture.srcEvent.preventDefault();
 		$('.p,.u').toggleClass('selected');
 		$('.previous,.upcoming').toggleClass('hide');
 	});
-	$('.right').hammer().on('tap',function(){
+	$('.right').hammer().off().on('tap',function(e){
+		e.gesture.srcEvent.preventDefault();
 		var id = $(this).parents('.option').attr('data-id');
 		$('#deleteModal').attr('data-id',id).modal();
 	});
-	$('#deleteModal .confirm').hammer().on('tap',function(){
+	$('#deleteModal .confirm').hammer().off().on('tap',function(e){
+		e.gesture.srcEvent.preventDefault();
 		$('#deleteModal').modal('hide');
 		var resId = $(this).parents('#deleteModal').attr('data-id');
 		CHelper.asynRequest('/book/cancel',{id:resId},{
