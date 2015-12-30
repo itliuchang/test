d0760a21f528a5bfabba2e2101cd7d200c9507fb
CallMEjun <394383890@qq.com>
2015-12-30 11:56:22 +0800

    fa

--- Web/protected/components/Controller.php
+++ Web/protected/components/Controller.php
@@ -19,6 +19,7 @@ class Controller extends CController{
 	 * for more details on how to specify this property.
 	 */
 	public $breadcrumbs = array();
+
 	public $bodyCss = '';
 	
 	public function init(){
