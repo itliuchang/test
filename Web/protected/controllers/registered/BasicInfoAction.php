2299a2cb402c6a804ccbc0665995719b8708dd60
刘畅 <liuchang@liuchangdeMacBook-Air.local>
2015-12-30 19:49:51 +0800

    fs

--- Web/protected/controllers/registered/BasicInfoAction.php
+++ Web/protected/controllers/registered/BasicInfoAction.php
@@ -3,7 +3,7 @@ class BasicInfoAction extends CAction{
 	public function run(){
 		if(Yii::app()->user->isGuest) {
 			if (Yii::app()->session['user']) {
-				$this->controller->render('/payment/wxpay/jsapi/');
+				$this->controller->redirect('/payment/wxpay/jsapi/');
 			}else{
 				$this->controller->render('basicInfo');
 			}
