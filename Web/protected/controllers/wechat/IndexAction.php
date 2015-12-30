80c7d553b5b33a351f41215532c4a45df39e95c7
刘畅 <liuchang@liuchangdeMacBook-Air.local>
2015-12-30 12:36:17 +0800

    fs

--- Web/protected/controllers/wechat/IndexAction.php
+++ Web/protected/controllers/wechat/IndexAction.php
@@ -1,13 +1,13 @@
 <?php
 class IndexAction extends CAction{
 	public function run(){
-		// if(Yii::app()->user->isGuest){
-		// 	$this->controller->bodyCss='access';
-		// 	$this->controller->redirect('/wechat/wechatconnect');
-		// }else{
-		// 	$this->controller->redirect(Assist::getAccessURL());
-		// }
-		$this->controller->render('index');
+		if(Yii::app()->user->isGuest){
+			$this->controller->bodyCss='access';
+			$this->controller->redirect('/wechat/wechatconnect');
+		}else{
+			$this->controller->redirect(Assist::getAccessURL());
+		}
+		// $this->controller->render('index');
 		
 	}
 }
