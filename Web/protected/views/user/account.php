ac11bde3b30e388dbaa03d36f2408e0ccd7e7766
刘畅 <liuchang@liuchangdeMacBook-Air.local>
2016-01-03 11:43:42 +0800

    fs

--- Web/protected/views/user/account.php
+++ Web/protected/views/user/account.php
@@ -1,5 +1,22 @@
 <div id="account">
 	<?php foreach($data['list'] as $item): ?>
+	<?php if($item['productType']==3): ?>
+		<div class="option nouse ">
+			<h3>OPEN DESK</h3>
+			<div class="content">
+				<div class="left">
+					<h4>Entries:Unlimited</h4>
+					<p>Product:<?php echo $item['productName'] ?></p>
+					<p>Duration:1 month</p>                             
+					<p>Vaild Date:<?php echo $item['startDate'] ?> - <?php echo $item['endDate'] ?></p>
+				</div>
+				<div class="right">
+						<h3 class="unlimited"></h3>
+						<p>unlimited</p>
+				</div>
+			</div>
+		</div>
+	<?php else: ?>
 	<div class="option nouse ">
 		<h3>OPEN DESK</h3>
 		<div class="content">
@@ -16,6 +33,7 @@
 			</div>
 		</div>
 	</div>
+	<?php endif; ?>
 	<?php endforeach; ?>
 	<a href="/registered/productlist" class="footer">ADD SUBSCRIPTION</a>
 </div>
