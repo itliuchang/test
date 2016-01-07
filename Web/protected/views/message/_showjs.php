8d9b5ac60bd4ab511d4c26c2fd692f968a913eae
刘畅 <liuchang@liuchangdeMacBook-Air.local>
2016-01-07 13:03:55 +0800

    fs

--- Web/protected/views/message/_showjs.php
+++ Web/protected/views/message/_showjs.php
@@ -1,7 +1,7 @@
 $.infinitRefresh({
     container: '#messageshow .wrapper', item: '.item',
     url: '/message/' + friendId + '/chat/%(start)s/%(size)s.html',
-    perPage: 15, data: null, callbacks: {
+    perPage: 5, data: null, callbacks: {
         success: function(response){
             if(typeof response.data == 'object' && $.isArray(response.data.list)){
                 var html = template('showTpl', {
