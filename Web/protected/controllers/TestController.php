d6792b0f103a994c5fcb7675969c62277bda8aba
刘畅 <liuchang@liuchangdeMacBook-Air.local>
2016-01-08 12:56:05 +0800

    fs

--- Web/protected/controllers/TestController.php
+++ Web/protected/controllers/TestController.php
@@ -1,8 +1,9 @@
 <?php
 class TestController extends Controller{
 	public function actionIndex(){
-		$user = new CReservation;
+		/*$user = new CReservation;
 		$result = $user->cancel(302);
-		print_r($result);die;
+		print_r($result);die;*/
+		print_r(Yii::app()->params['partner']['wechat']);die;
 	}
 }
--- Web/protected/vendor/wxpayAPI/lib/WxPay.Api.php
+++ Web/protected/vendor/wxpayAPI/lib/WxPay.Api.php
@@ -47,7 +47,7 @@ class WxPayApi
 		if(!$inputObj->IsNotify_urlSet()){
 			$inputObj->SetNotify_url(WxPayConfig::NOTIFY_URL);//异步通知url
 		}
-		
+		Yii::log(WxPayConfig::MCHID, CLogger::LEVEL_ERROR,'mchid');
 		$inputObj->SetAppid(WxPayConfig::APPID);//公众账号ID
 		$inputObj->SetMch_id(WxPayConfig::MCHID);//商户号
 		$inputObj->SetSpbill_create_ip($_SERVER['REMOTE_ADDR']);//终端ip	  
