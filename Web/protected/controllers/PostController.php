60145e2136e966e663660fb62e8a9ec4647ad527
刘畅 <liuchang@liuchangdeMacBook-Air.local>
2016-01-08 16:20:16 +0800

    fs

--- Web/protected/controllers/PostController.php
+++ Web/protected/controllers/PostController.php
@@ -1,5 +1,35 @@
 <?php
 class PostController extends Controller{
+
+	public function filters(){
+        return array(
+            'accessControl',
+        );
+    }
+
+    public function accessRules(){
+        return array(
+            // array('allow',
+            //     'actions'=>array(
+            //         'wechatquietconnect', 'wechatquietconnectcallback',
+            //     ),
+            //     'users'=>array('?'),
+            // ),
+            array('allow',
+                'actions'=>array(
+                    'postshow','newpost'
+                ),
+                'users'=>array('*'),
+            ),
+            array('allow',
+                'actions' => array('newlist',),
+                'users' => array('@'),
+            ),
+            array('deny',
+               'users' => array('*'),
+            ),
+        );
+    }
 	public function actions(){
 		return array(
 			'newlist' => 'application.controllers.post.NewListAction',
