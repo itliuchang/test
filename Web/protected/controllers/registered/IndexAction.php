a26f5f89723f2369536f9de898e5c9e2d65fa530
刘畅 <liuchang@liuchangdeMacBook-Air.local>
2016-01-08 16:11:41 +0800

    fs

--- Web/protected/controllers/registered/IndexAction.php
+++ Web/protected/controllers/registered/IndexAction.php
@@ -2,12 +2,13 @@
 class IndexAction extends CAction{
 	public function run(){
 		if(Yii::app()->user->isGuest){
+
 			$this->controller->bodyCss='access';
 			if(!Yii::app()->session['wechat']) {
 				$this->controller->redirect('/wechat/wechatconnect');
 			}
 		}else{
-			$this->controller->redirect(Assist::getDefaultURL());
+			$this->controller->redirect(Assist::getAccessURL());
 		}
 		
 	}
