0e598d7fa8694b471099978c76cab687e63580fd
刘畅 <liuchang@liuchangdeMacBook-Air.local>
2016-01-09 13:09:49 +0800

    fs

--- Web/protected/controllers/TestController.php
+++ Web/protected/controllers/TestController.php
@@ -1,10 +1,8 @@
 <?php
 class TestController extends Controller{
 	public function actionIndex(){
-		$this->pageTitle='hehe';
-		/*$user = new CReservation;
-		$result = $user->cancel(302);
-		print_r($result);die;*/
-		print_r(Yii::app()->params['partner']['wechat']);die;
+		$user = new CReservation;
+		$result = $user->cancel(322);
+		print_r($result);die;
 	}
 }
