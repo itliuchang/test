3318375a79eb2680c9a5ed24f34ddd40cf48dd47
刘畅 <liuchang@liuchangdeMacBook-Air.local>
2016-01-20 09:59:02 +0800

    fs

--- Web/protected/controllers/TestController.php
+++ Web/protected/controllers/TestController.php
@@ -1,8 +1,10 @@
 <?php
 class TestController extends Controller{
 	public function actionIndex(){
-		$order=new COrder;
-		$result = $order->update(596);
-		print_r($result);die;
+		// $order=new COrder;
+		// $result = $order->update(596);
+		// print_r($result);die;
+		$date = date('U');
+		echo date("Y/m/d",$date+Assist::timestampToMonthTimestamp($date)-86400);
 	}
 }
