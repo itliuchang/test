dd7e0f08e59a48623cf6d8f7f0142ecc901e9c74
刘畅 <liuchang@liuchangdeMacBook-Air.local>
2016-01-21 15:59:30 +0800

    fs

--- Web/protected/views/registered/_chooseEnvironmentjs.php
+++ Web/protected/views/registered/_chooseEnvironmentjs.php
@@ -9,7 +9,7 @@ $(function(){
 			},
 			success:function(data){
 				CHelper.toggleTip('show','success','success','2000');
-				location.href="/code-"+data.code;
+				setInterval(location.href="/code-"+data.code, 2000);
 			},
 			error:function(res){
 				CHelper.toggleTip('show','error','error','2000');
